1b6f5271bfc0936d371159b83d1b4b8ba15d25d6
tadef <tadef@DESKTOP-F3LITGC>
2023-09-11 20:48:07 -0300

    TF - Se genera Registro y Login de usuario

--- Agencia.cs
+++ Agencia.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using tpAgencia_Gpo_2;
-//hola
 
 public class Agencia
 {
@@ -10,10 +9,47 @@ public class Agencia
 	private List<Ciudad> Ciudades;
 	private List<ReservaHotel> ReservasHotel;
 	private List<ReservaVuelo> ReservasVuelo;
-	private Usuario UsuarioActual;
+	private Usuario UsuarioActual { get; set; };
 
 	//metodo constructor
 	public Agencia()
 	{
 	}
+
+
+    public void registrarUsuario(int id, string name, string apellido, int dni, string mail, string password, bool esAdmin)
+    {
+        Usuario usuario = new Usuario(id,name,apellido,dni,mail, password, esAdmin);
+        Usuarios.add(usuario);
+    }
+
+    public boolean IniciarSesion(String mail, String password)
+    {
+        for (Usuario usuario : Usuarios)
+        {
+            if (usuario.getMail().equals(mail) && usuario.getPassword().equals(password))
+            {
+                if (!usuario.isBloqueado())
+                {
+                    setUsuarioActual(usuario);
+                    return true; // Sesión iniciada con éxito
+                }
+                else
+                {
+                    return false; // Usuario bloqueado
+                }
+            }
+        }
+        // Si no se encuentra el usuario o la contraseña es incorrecta se agrega un intento fallido
+        for (Usuario usuario : Usuarios)
+        {
+            if (usuario.getMail().equals(mail))
+            {
+                usuario.agregarIntentoFallido();
+                return false;
+            }
+        }
+
+        return false; // Usuario no encontrado
+    }
 }
--- Usuario.cs
+++ Usuario.cs
@@ -7,7 +7,7 @@
         public string apellido { get; set; }
         public int dni { get; set; }
         public string mail { get; set; }
-        public string pasword { get; set; }
+        public string password { get; set; }
         public int intentosFallidos { get; set; }
         public Boolean bloqueado { get; set; }
         private List<ReservaHotel> misReservasHoteles {  get; set; }
@@ -23,15 +23,26 @@
         {
         }
 
-        public Usuario(int id, string name, string apellido, int dni, bool esAdmin)
+        public Usuario(int id, string name, string apellido, int dni,string mail,string password, bool esAdmin)
         {
             this.id = id;
             this.name = name;
             this.apellido = apellido;
             this.dni = dni;
+            this.mail = mail;  
+            this.password = password;   
             this.esAdmin = false;
         }
 
         //metodos
+
+        public void agregarIntentoFallido()
+        {
+            intentosFallidos++;
+            if (intentosFallidos >= 3)
+            {
+                bloqueado = true;
+            }
+        }
     }
 }
