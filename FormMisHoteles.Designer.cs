4ec5b7fdcf400a57a20c7767b81bd9c6e1a45b8c
tadef <tadef@DESKTOP-F3LITGC>
2023-09-27 23:44:40 -0300

    TF - Agregado de MisHoteles reporte y en agencia agregue una funcion para mostrar Hoteles

---
+++ FormMisHoteles.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace tpAgencia_Gpo_2
+{
+    partial class FormMisHoteles
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            dataGridView1 = new DataGridView();
+            Nombre = new DataGridViewTextBoxColumn();
+            Ciudad = new DataGridViewTextBoxColumn();
+            Huespedes = new DataGridViewTextBoxColumn();
+            Costo = new DataGridViewTextBoxColumn();
+            FechaDesde = new DataGridViewTextBoxColumn();
+            FechaHasta = new DataGridViewTextBoxColumn();
+            Volver_desde_usuario = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Cascadia Code", 26F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(253, 37);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(251, 46);
+            label1.TabIndex = 1;
+            label1.Text = "Mis Hoteles";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(33, 137);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(98, 13);
+            label2.TabIndex = 2;
+            label2.Text = "Historial de Hoteles";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Nombre, Ciudad, Huespedes, Costo, FechaDesde, FechaHasta });
+            dataGridView1.Location = new Point(11, 181);
+            dataGridView1.Margin = new Padding(2);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.RowTemplate.Height = 33;
+            dataGridView1.Size = new Size(745, 175);
+            dataGridView1.TabIndex = 3;
+            // 
+            // Nombre
+            // 
+            Nombre.HeaderText = "Nombre";
+            Nombre.Name = "Nombre";
+            // 
+            // Ciudad
+            // 
+            Ciudad.HeaderText = "Ciudad";
+            Ciudad.Name = "Ciudad";
+            // 
+            // Huespedes
+            // 
+            Huespedes.HeaderText = "Huespedes";
+            Huespedes.Name = "Huespedes";
+            // 
+            // Costo
+            // 
+            Costo.HeaderText = "Costo";
+            Costo.Name = "Costo";
+            // 
+            // FechaDesde
+            // 
+            FechaDesde.HeaderText = "FechaDesde";
+            FechaDesde.Name = "FechaDesde";
+            // 
+            // FechaHasta
+            // 
+            FechaHasta.HeaderText = "FechaHasta";
+            FechaHasta.Name = "FechaHasta";
+            // 
+            // Volver_desde_usuario
+            // 
+            Volver_desde_usuario.BackColor = Color.FromArgb(192, 255, 255);
+            Volver_desde_usuario.Location = new Point(56, 394);
+            Volver_desde_usuario.Name = "Volver_desde_usuario";
+            Volver_desde_usuario.Size = new Size(75, 23);
+            Volver_desde_usuario.TabIndex = 28;
+            Volver_desde_usuario.Text = "Volver";
+            Volver_desde_usuario.UseVisualStyleBackColor = false;
+            // 
+            // FormMisHoteles
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(Volver_desde_usuario);
+            Controls.Add(dataGridView1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "FormMisHoteles";
+            Text = "FormMisHoteles";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Nombre;
+        private DataGridViewTextBoxColumn Ciudad;
+        private DataGridViewTextBoxColumn Huespedes;
+        private DataGridViewTextBoxColumn Costo;
+        private DataGridViewTextBoxColumn FechaDesde;
+        private DataGridViewTextBoxColumn FechaHasta;
+        private Button Volver_desde_usuario;
+    }
+}
---
+++ FormMisHoteles.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static tpAgencia_Gpo_2.BuscadorVuelos;
+
+namespace tpAgencia_Gpo_2
+{
+    public partial class FormMisHoteles : Form
+    {
+
+        private Agencia agencia;
+        private Form1 form1;
+        private Usuario usuarioActual;
+        public TransfDelegadoMisHoteles TransfEventoMisVuelos;
+        public FormMisHoteles(Agencia agencia, Form1 form1)
+        {
+            InitializeComponent();
+            this.agencia = agencia;
+            this.form1 = form1;
+            usuarioActual = agencia.getUsuarioActual();
+            this.Load += FormMisHoteles_Load;
+        }
+
+        public delegate void TransfDelegadoMisHoteles();
+
+        private void FormMisHoteles_Load(object sender, EventArgs e)
+        {
+            MostrarHoteles();
+        }
+        private void MostrarHoteles()
+        {
+            dataGridView1.Rows.Clear();
+
+            if (usuarioActual != null)
+            {
+                List<Hotel> hotelesPasados = agencia.misHoteles(usuarioActual);
+
+                foreach (var hoteles in hotelesPasados)
+                {
+                    Hotel hotel = hotelesPasados.FirstOrDefault(v => v.id == hoteles.Hotel);
+                    if (hotel != null)
+                    {
+
+                        dataGridView1.Rows.Add(
+                            hotel.ubicacion,
+                            hotel.capacidad,
+                            hotel.costo,
+                            hotel.nombre,
+                            hotel.huespedes
+                            );
+                    }
+
+                }
+
+
+
+            }
+
+        }
+
+        private void Volver_desde_usuario_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            MenuAgencia MenuAgencia = new MenuAgencia(agencia, form1);
+            MenuAgencia.MdiParent = form1;
+            MenuAgencia.Show();
+        }
+    }
+}
