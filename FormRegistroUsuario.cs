936345ec0ff0115c0a95ddded7089a379fcbb394
Gaston Rambo <gaston_63ramos@hotmail.com>
2023-10-09 22:40:09 -0300

    agrego el Form de Registro de Usuario

---
+++ FormRegistroUsuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tpAgencia_Gpo_2
+{
+    public partial class FormRegistroUsuario : Form
+    {
+        public FormRegistroUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Registrar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
